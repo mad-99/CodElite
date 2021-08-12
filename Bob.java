5fcc2a3f0910c34e40aefad0f2811653f9a8d154
mad-99 <madhurmaheshwari1999@gmail.com>
2021-08-12 22:34:51 +0530

    main

---
+++ Bob.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class Bob{
+    public static boolean isValidIFSC(String s){
+        if(s.length() != 11){
+            return false;
+        }
+        for(int i=0;i<4;i++){
+            int ch = (int) s.charAt(i);
+            if(ch<65 || ch>90){
+                return false;
+            }
+        }
+        if(s.charAt(4) != '0'){
+            return false;
+        }
+        for(int i=0;i<11;i++){
+            int ch = (int) s.charAt(i);
+            if((ch >= 65 && ch<= 90)|| (ch >= 48 && ch <= 57)){
+                continue;
+            }
+            return false;
+        }
+        return true;
+        
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        if(isValidIFSC(s)==true){
+            System.out.println("True");
+        }else{
+        System.out.println("False");
+        }
+    }
+}
